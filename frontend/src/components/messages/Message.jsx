a1aa5e5172e6996666074a53c7fc47a0b0ca4042
Muzammil-cyber <muzammilloya@hotmail.com>
2024-04-03 17:42:16 +0500

    Add MessageSkeleton component and update Message and Messages components

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,19 +1,27 @@
-const Message = () => {
+import { useAuthContext } from "../../context/AuthContext";
+import useConversation from "../../store/useConversation";
+import { extractTime } from "../../utils/time";
+
+const Message = ({ message }) => {
+  const { user } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === user.id;
+  const profilePic =
+    message.senderId === user.id
+      ? user.profilePic
+      : selectedConversation.profilePic;
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${fromMe ? "chat-end" : "chat-start"}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS chat bubble"
-            src="https://randomuser.me/api/portraits/men/95.jpg"
-          />
+          <img alt="Tailwind CSS chat bubble" src={profilePic} />
         </div>
       </div>
-      <div className="chat-bubble text-white bg-blue-500 ">
-        Hi! What is upp?
+      <div className={`chat-bubble text-white ${fromMe ? "bg-blue-500" : ""}`}>
+        {message.message}
       </div>
-      <div className="chat-footer opacity-50 flex gap-1 items-center text-xs">
-        10:00
+      <div className="chat-footer opacity-50 flex gap-1 items-center text-xs pb-2">
+        {extractTime(message.createdAt)}
       </div>
     </div>
   );
