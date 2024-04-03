ff6f2a57615b94eab5f480cc55040e7753ce1d19
Muzammil-cyber <muzammilloya@hotmail.com>
2024-04-04 01:16:08 +0500

    Update function to handle edge case

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -36,6 +36,7 @@ export const SocketProvider = ({ children }) => {
         setSocket(null);
       }
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user]);
 
   return (
