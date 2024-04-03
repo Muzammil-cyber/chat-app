f096359dbccf4e9016677548457f7a4b3029981d
Muzammil-cyber <muzammilloya@hotmail.com>
2024-04-04 00:56:06 +0500

    Update socket and proxy URLs

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (user) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chat-app-snowy-ten.vercel.app", {
         query: {
           userId: user._id,
         },
